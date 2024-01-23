0bbea9f8778480e6dd146f520cc6cd7d41218b43
vinaykumar0 <67115920+vinaykumar0@users.noreply.github.com>
2024-01-23 11:07:07 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,26 +2,6 @@ const express = require('express')
 const { signUpValidator, loginValidator } = require('../helpers/validations')
 const { signUp , login } = require('../controllers/userController')
 
-// const multer = require('multer')
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null,path.join(__dirname,'../public/image'))
-//     },
-//     filename: function (req, file, cb) {
-//         const name = Date.now() + '-' + file.originalname
-//         cb(null,name)
-//     }
-// })
-
-// const filefilter = (req, file, cb) => {
-//     (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') ?
-//         cb(null,true):cb(null,false)
-// }
-
-// const upload=multer({storage:storage,fileFilter:filefilter})
-
-
 const router = express.Router()
 
 router.post('/signup', signUpValidator, signUp)
